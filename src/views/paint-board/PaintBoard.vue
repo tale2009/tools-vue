9b8ac586838e9aa08eefb0fa86196a25bfc99d44
kooriookami <bingshuanglingluo@163.com>
2021-03-30 14:38:52 +0800

    feat(paint-board): 添加绘图工具

--- src/views/paint-board/PaintBoard.vue
+++ src/views/paint-board/PaintBoard.vue
@@ -29,13 +29,29 @@
                         <el-form-item label="高度">
                             <el-input-number v-model="form.height" :min="100" :max="9999" :precision="0" @change="resetSize"></el-input-number>
                         </el-form-item>
+                        <el-form-item label="工具">
+                            <el-space :size="10" wrap>
+                                <el-check-tag :checked="form.type === 'pencil'" @change="changeType('pencil')">铅笔</el-check-tag>
+                                <el-check-tag :checked="form.type === 'text'" @change="changeType('text')" v-if="false">文本</el-check-tag>
+                                <el-check-tag :checked="form.type === 'eraser'" @change="changeType('eraser')">橡皮擦</el-check-tag>
+                            </el-space>
+                        </el-form-item>
+                        <el-form-item label="形状">
+                            <el-space :size="10" wrap>
+                                <el-check-tag :checked="form.type === 'straight'" @change="changeType('straight')">直线</el-check-tag>
+                                <el-check-tag :checked="form.type === 'rectangle'" @change="changeType('rectangle')">矩形</el-check-tag>
+                                <el-check-tag :checked="form.type === 'ellipse'" @change="changeType('ellipse')">圆形</el-check-tag>
+                            </el-space>
+                        </el-form-item>
                         <el-form-item label="颜色">
                             <el-color-picker v-model="form.color"></el-color-picker>
                         </el-form-item>
                         <el-form-item label="线宽">
-                            <el-slider v-model="form.lineWidth" :min="1" :max="10"></el-slider>
+                            <el-slider v-model="form.lineWidth" :min="1" :max="30"></el-slider>
                         </el-form-item>
                     </el-form>
+
+                    <el-button style="width: 100%" type="primary" size="medium" @click="exportImage">导出图片</el-button>
                 </div>
             </template>
         </Page>
@@ -44,7 +60,9 @@
 
 <script>
     import Page from '@/components/page/Page';
-    import {computed, onBeforeUnmount, onMounted, reactive, ref} from 'vue';
+    import {computed, getCurrentInstance, onBeforeUnmount, onMounted, reactive, ref} from 'vue';
+    import html2canvas from '@/assets/js/html2canvas';
+    import usePaint from '@/views/paint-board/use-paint';
 
     export default {
         name: 'PaintBoard',
@@ -52,16 +70,27 @@
             Page
         },
         setup() {
+            const {ctx} = getCurrentInstance();
             const canvas = ref(null);
-            let context;
+            const context = ref(null);
             const form = reactive({
                 width: 1920,
                 height: 1080,
+                type: 'pencil',
                 color: '#000',
                 lineWidth: 3
             });
-            const lastPoint = ref({});
+            const downPoint = ref({}); // 鼠标按钮坐标
+            const lastPoint = ref({});  // 上一次的坐标
             const historyList = ref([]); // 保存历史图像
+
+            const usePaintKey = {
+                context,
+                form,
+                downPoint,
+                lastPoint,
+                historyList
+            };
 
             onMounted(() => {
                 createPaintBoard();
@@ -72,13 +101,10 @@
             });
 
             const createPaintBoard = () => {
-                context = canvas.value.getContext('2d');
+                context.value = canvas.value.getContext('2d');
 
                 canvas.value.width = form.width;
                 canvas.value.height = form.height;
-
-                context.lineCap = 'round';
-                context.lineJoin = 'round';
 
                 canvas.value.addEventListener('mousedown', onMousedown);
             };
@@ -91,63 +117,62 @@
             });
 
             const resetSize = () => {
-                const history = context.getImageData(0, 0, canvas.value.width, canvas.value.height);
+                const history = context.value.getImageData(0, 0, canvas.value.width, canvas.value.height);
 
                 canvas.value.width = form.width;
                 canvas.value.height = form.height;
 
-                context.putImageData(history, 0, 0);
+                context.value.putImageData(history, 0, 0);
             };
 
-            const drawLine = (x, y) => {
-                context.beginPath();
-                if (Object.keys(lastPoint.value).length) {
-                    context.moveTo(lastPoint.value.x, lastPoint.value.y);
-                } else {
-                    context.moveTo(x, y);
-                }
-                context.lineTo(x, y);
-                lastPoint.value = {x: x, y: y};
-                context.lineWidth = form.lineWidth;
-                context.strokeStyle = form.color;
-                context.stroke();
+            const changeType = type => {
+                form.type = type;
             };
 
             const saveHistory = () => {
-                const history = context.getImageData(0, 0, canvas.value.width, canvas.value.height);
+                const history = context.value.getImageData(0, 0, canvas.value.width, canvas.value.height);
                 historyList.value.push(history);
             };
 
             const restoreHistory = () => {
                 const history = historyList.value.pop();
-                context.putImageData(history, 0, 0);
+                context.value.putImageData(history, 0, 0);
             };
 
             const clearPaintBoard = () => {
-                context.clearRect(0, 0, canvas.value.width, canvas.value.height);
+                context.value.clearRect(0, 0, canvas.value.width, canvas.value.height);
                 historyList.value = [];
             };
 
             const onMousedown = e => {
+                const x = e.offsetX;
+                const y = e.offsetY;
                 addEventListener('mousemove', onMousemove);
                 addEventListener('mouseup', onMouseup);
                 saveHistory();
-                lastPoint.value = {x: e.offsetX, y: e.offsetY};
-                drawLine(e.offsetX, e.offsetY);
+                downPoint.value = {x, y};
+                lastPoint.value = {x, y};
+                usePaint(usePaintKey, form.type, e);
 
                 document.onselectstart = () => false;
                 document.ondragstart = () => false;
             };
 
             const onMousemove = e => {
+                const x = e.offsetX;
+                const y = e.offsetY;
                 if (e.target === canvas.value) {
-                    drawLine(e.offsetX, e.offsetY);
+                    usePaint(usePaintKey, form.type, e);
+                    lastPoint.value = {x, y};
                 } else {
                     lastPoint.value = {};
                 }
             };
 
             const onMouseup = () => {
+                downPoint.value = {};
+                lastPoint.value = {};
+
                 removeEventListener('mousemove', onMousemove);
                 removeEventListener('mouseup', onMouseup);
 
@@ -155,15 +180,32 @@
                 document.ondragstart = null;
             };
 
+            const exportImage = () => {
+                let element = document.querySelector('.paint-board');
+                html2canvas(element, {
+                    backgroundColor: 'transparent',
+                    onclone: doc => {
+                        let paintBoard = doc.querySelector('.paint-board');
+                        paintBoard.style.boxShadow = 'none';
+                    }
+                }).then(canvas => {
+                    let dataURL = canvas.toDataURL('image/png', 1);
+                    let blob = ctx.dataURLtoBlob(dataURL);
+                    ctx.downloadBlob(blob, '画图板');
+                });
+            };
+
             return {
                 canvas,
+                context,
                 form,
-                lastPoint,
                 historyList,
                 paintBoardStyle,
                 restoreHistory,
                 clearPaintBoard,
-                resetSize
+                resetSize,
+                changeType,
+                exportImage
             };
         }
     };
---
+++ src/views/paint-board/use-paint.js
@@ -0,0 +1,88 @@
+const usePaint = (usePaintKey, type, e) => {
+    const {
+        context,
+        form,
+        downPoint,
+        lastPoint,
+        historyList
+    } = usePaintKey;
+
+    const history = historyList.value[historyList.value.length - 1];
+
+    const x = e.offsetX;
+    const y = e.offsetY;
+    context.value.lineCap = 'butt';
+    context.value.lineJoin = 'miter';
+    context.value.lineWidth = form.lineWidth;
+    context.value.strokeStyle = form.color;
+
+    const usePencil = () => {
+        context.value.beginPath();
+        if (Object.keys(lastPoint.value).length) {
+            context.value.moveTo(lastPoint.value.x, lastPoint.value.y);
+        } else {
+            context.value.moveTo(x, y);
+        }
+        context.value.lineTo(x, y);
+        context.value.lineCap = 'round';
+        context.value.lineJoin = 'round';
+        context.value.stroke();
+    };
+
+    const useText = () => {
+
+    };
+
+    const useEraser = () => {
+        context.value.clearRect(x, y, form.lineWidth, form.lineWidth);
+    };
+
+    const useStraight = () => {
+        context.value.putImageData(history, 0, 0);
+        context.value.beginPath();
+        context.value.moveTo(downPoint.value.x, downPoint.value.y);
+        context.value.lineTo(x, y);
+        context.value.lineCap = 'round';
+        context.value.lineJoin = 'round';
+        context.value.stroke();
+    };
+
+    const useRectangle = () => {
+        context.value.putImageData(history, 0, 0);
+        context.value.strokeRect(downPoint.value.x, downPoint.value.y, x - downPoint.value.x, y - downPoint.value.y);
+    };
+
+    const useEllipse = () => {
+        context.value.putImageData(history, 0, 0);
+        context.value.beginPath();
+        let radiusX = Math.abs((x - downPoint.value.x) / 2);
+        let radiusY = Math.abs((y - downPoint.value.y) / 2);
+        let centerX = radiusX + Math.min(downPoint.value.x, x);
+        let centerY = radiusY + Math.min(downPoint.value.y, y);
+        context.value.ellipse(centerX, centerY, radiusX, radiusY, 0, 0, Math.PI * 2);
+        context.value.stroke();
+    };
+
+    switch (type) {
+        case 'pencil':
+            usePencil();
+            break;
+        case 'text':
+            useText();
+            break;
+        case 'eraser':
+            useEraser();
+            break;
+        case 'straight':
+            useStraight();
+            break;
+        case 'rectangle':
+            useRectangle();
+            break;
+        case 'ellipse':
+            useEllipse();
+            break;
+    }
+};
+
+export default usePaint;
