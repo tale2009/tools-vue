1553158e44e44cd57b3d202d1fd80ce2f737fec6
kooriookami <bingshuanglingluo@163.com>
2021-08-04 20:41:31 +0800

    移动端提示

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+    import mobile from 'is-mobile';
     import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 
     export default {
@@ -13,6 +14,20 @@
             return {
                 locale: zhCn
             };
+        },
+        mounted() {
+            this.mobileTip();
+        },
+        methods: {
+            mobileTip() {
+                if (mobile()) {
+                    this.$confirm('<p>若功能异常，请使用 PC Chrome 浏览器。</p>' +
+                        '<p>移动端异常问题不再回复，请谅解！</p>', '移动端提示', {
+                        type: 'warning',
+                        dangerouslyUseHTMLString: true
+                    });
+                }
+            }
         }
     };
 </script>
