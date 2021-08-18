0d99ee944c0af6ea246baaff53ed0ca93c7700a1
kooriookami <bingshuanglingluo@163.com>
2021-08-18 23:25:00 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
         },
         methods: {
             mobileTip() {
-                if (mobile({tablet: true})) {
+                if (mobile()) {
                     this.$confirm('<p>若功能异常，请使用 PC Chrome 浏览器。</p>' +
                         '<p>移动端异常问题不再回复，请谅解！</p>', '移动端提示', {
                         type: 'warning',
